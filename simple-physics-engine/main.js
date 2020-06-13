dcbec731bcdf5d700e0738a9ab1ef52a9bc6626b
Mladen Krivacevic <mladen.krivacevic@gmail.com>
2020-06-13 19:50:30 +0200

    Created an example for Webpack + Babel

--- simple-physics-engine/main.js
+++
@@ -1,4 +0,0 @@
-import { Circle } from "./circle.js";
-
-const circle1 = new Circle(3, 2, 5);
-console.log("The Center of circle1 is at: ", circle1.getPosition());
---
+++ webpackbabelmodules/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path'); // "path" je tzv. "core" modul tj. ne moramo ga instalirati ili sl.
+             // Sluzi za manipulisanje putanja (do) fajlova
+
+module.exports = {
+  // Ovdje definisemo ulaznu tacku
+  entry: {
+    app: './main.js' // Fajl koji zelimo da uzmemo da nam bude "izvorni" fajl (ES6/ES7 kod...)
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'), // "__dirname" oznacava trenutni folder
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{ // Ovdje definisemo "loader"-e (kao npr. babel-loader)
+      test: /\.js?$/, // Regularni izraz
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: { // Ovdje definisemo "presets"
+        presets: ['@babel/preset-env']
+      }
+    }]
+  }
+}
